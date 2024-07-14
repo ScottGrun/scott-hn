76d4292572e8530fa047abe3155b3424a310e8f6
Scott Grunerud <scottgrunerud98@gmail.com>
2024-07-13 18:47:48 -0700

    attempt to fix prod issue with dompurify pt3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
 	plugins: [
+		nodePolyfills({}),
 		sveltekit(),
 		svg({
 			includePaths: ['./src/lib/assets/icons/']
